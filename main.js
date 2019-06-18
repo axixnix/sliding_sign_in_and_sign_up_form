9a1efd7fea00549f7e78f1fc3cbd9e701f720a2c
axixnix <henryirons@yahoo.com>
2019-06-18 01:22:53 +0100

    added event listeners for the signin and signup buttons, was getting some errors so i moved the js script tag to the last line within the body tag

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById('signUp')
+const signInButton = document.getElementById('signIn')
+const container = document.getElementById('container')
+
+signUpButton.addEventListener('click', () => container.classList.add('right-panel-active')) // this and the one beneath this comment 
+// were giving me an uncaught error because my js script tag was in the head element 
+// instead of just above the closing of the body element , the error was as a result of my script running in the
+// head and referencing ids in elements yet to be created
+
+signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'))
